d2b6cc6a124112374301faa08d2526c9ebfd2102
brian-paiba <brianmanuelpaiba@outlook.es>
2025-05-14 23:24:56 -0300

    refactor: remove unused components and simplify project structure

---
+++ calculator-app/app/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function CalculatorApp () {
+  return (
+    <View>
+      <Text>CalculatorApp </Text>
+    </View>
+  )
+}
