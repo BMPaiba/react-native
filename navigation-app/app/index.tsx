f76fd26098e59f563af98e6a4acaa74d5a75d0fb
brian-paiba <brianmanuelpaiba@outlook.es>
2025-05-20 18:19:55 -0300

    feat: agregar configuración de Babel y Metro, y archivos de estilo global
    Este commit introduce la configuración necesaria para Babel y Metro, incluyendo:
    - Archivo babel.config.js para la integración de NativeWind.
    - Archivo metro.config.js para la configuración de Metro con soporte para estilos globales.
    - Archivo tailwind.config.js para la configuración de Tailwind CSS.
    - Archivo global.css para definir estilos globales.
    - Se agrega un archivo de declaración de tipos para NativeWind.
    - Se actualiza tsconfig.json para incluir el nuevo archivo de declaración.
    - Se modifica el componente RootLayout para utilizar estilos globales y se eliminan componentes obsoletos.

---
+++ navigation-app/app/index.tsx
@@ -0,0 +1,19 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Hola Mundoxcvxcvxcv
+
+
+
+        l;kansdfk;sdjnf
+
+
+        amsd'l;aksdll'skndf;lskdnf
+      </Text>
+    </View>
+  )
+}
